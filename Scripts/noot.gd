79857f88644feea7b2427a5e3d0a742bbe76263c
Username4D <studio@kremzow.org>
2025-09-19 20:42:13 +0200

    added shop ui

--- Scripts/noot.gd
+++ Scripts/noot.gd
@@ -1,6 +1,10 @@
 extends AnimatedSprite2D
 
 func _input(event):
-	if event is InputEventKey and event.pressed:
+	if event is InputEventKey and event.pressed and not event.is_echo():
 		self.frame = 0
 		self.play("default")
+		UiHandler.coins += 1
+
+func _process(delta: float) -> void:
+	$Label.text = var_to_str(UiHandler.coins) + " Coins"
---
+++ Scripts/product_category.gd
@@ -0,0 +1,4 @@
+extends Control
+
+func _ready() -> void:
+	$Label.text = self.name + ":"

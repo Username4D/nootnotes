10f0ac0489bd15fcc7f237e6de4ea7a448c3f0f4
Username4D <studio@kremzow.org>
2025-09-21 20:51:35 +0200

    added refreshing to shop

--- Scripts/shop.gd
+++ Scripts/shop.gd
@@ -2,3 +2,13 @@ extends Control
 
 func _process(delta: float) -> void:
 	$coins.text = "Balance: " + var_to_str(UiHandler.coins) + " Coins"
+func _ready() -> void:
+	PurchaseManager.reload_navbar.connect(update)
+	PurchaseManager.reload_colors.connect(update)
+func update():
+	for i in $ScrollContainer/VBoxContainer.get_children():
+		if i is Panel:
+			if i.is_color and PurchaseManager.bought_colors.find(i.color) != -1:
+				i.queue_free()
+			elif PurchaseManager.bought_features.find(i.name) != -1:
+				i.queue_free()
